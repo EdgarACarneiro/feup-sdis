f073bead66b17ce8fb5cfb9a86d2ece7f9430002
EdgarACarneiro <up201503784@fe.up.pt>
2018-02-15 01:42:43 +0000

    Testing :zzz:

--- Client-ServerUDP/src/com/src/Application.java
+++ Client-ServerUDP/src/com/src/Application.java
@@ -1,5 +1,6 @@
 package com.src;
 
+import java.io.IOException;
 import java.util.Scanner;
 import java.net.*;
 
@@ -8,42 +9,60 @@ import java.net.*;
  * Class responsible for interpreting the console input messages and then delegate them to either the Server or the Clients
  */
 public class Application {
-	
+
 	/**
 	 * The application's server
 	 */
 	private static Server server;
-	
+
 	/**
 	 * Possible States the Application may be in
 	 */
 	private enum State {
 		START, OPENED_SERVER
 	}
-	
+
 	/**
 	 * Current State the application is in
 	 */
-	private State currentState;
-	
-	public static void main(String [] args) {
+	private static State currentState;
+
+	public static void main(String[] args) {
 		//Inicializar a maquina de estados que assegura que o programa:
 		// 1 - abre o servidor (1� estado)
 		// 2 - chamadas consecutivas de cleinetes ( 2� estado)
-		
+
 		//Depois mudar isto para ele interpretar, para j� fica hardcoded para garantir que funciona
 		// send request
 
-		DatagramSocket socket = new DatagramSocket();
-		byte[] sbuf = 84;
-		InetAddress address = InetAddress.getByName(9823);
+		DatagramSocket socket = null;
+		try {
+			socket = new DatagramSocket();
+		} catch (SocketException e) {
+			e.printStackTrace();
+		}
+		byte[] sbuf = "test".getBytes();
+		InetAddress address = null;
+		try {
+			address = InetAddress.getByName("128.92.232.2");
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
 		DatagramPacket packet = new DatagramPacket(sbuf, sbuf.length, address, 4445);
-		socket.send(packet);
-		
+		try {
+			socket.send(packet);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 		// get response
 		byte[] rbuf = new byte[sbuf.length];
 		packet = new DatagramPacket(rbuf, rbuf.length);
-		socket.receive(packet);
+		try {
+			socket.receive(packet);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		// display response
 		String received = new String(packet.getData());
 		System.out.println("Echoed Message: " + received);
