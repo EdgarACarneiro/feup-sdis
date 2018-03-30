fc7ebf474a6d70aef8900eeb2ce6bddbde18d3df
EdgarACarneiro <up201503784@fe.up.pt>
2018-03-30 03:35:56 +0100

    Removed Thread.sleep, using schedule thread pools

--- Distributed-Backup-Service/src/Action/TriggerDeleteAction.java
+++ Distributed-Backup-Service/src/Action/TriggerDeleteAction.java
@@ -1,18 +1,18 @@
 package Action;
 
-import Messages.GetchunkMsg;
-import Channel.RestoreChannel;
+import Channel.ControlChannel;
+import Messages.DeleteMsg;
 import Messages.Message;
 import Utils.*;
 
 import java.util.ArrayList;
 
-public class RestoreAction extends ActionHasReply {
+public class TriggerDeleteAction extends Action {
 
     /**
      * The channel used to communicate with other peers, regarding backup files
      */
-    private RestoreChannel restoreChannel;
+    private ControlChannel controlChannel;
 
     /**
      * Protocol Version in the communication
@@ -34,8 +34,8 @@ public class RestoreAction extends ActionHasReply {
      */
     private ArrayList<byte[]> chunks = new ArrayList<>();
 
-    public RestoreAction(RestoreChannel restoreChannel, float protocolVersion, int senderID, String file) {
-        this.restoreChannel = restoreChannel;
+    public TriggerDeleteAction(ControlChannel controlChannel, float protocolVersion, int senderID, String file) {
+        this.controlChannel = controlChannel;
         this.protocolVersion = protocolVersion;
         this.senderID = senderID;
         this.fileID = FileManager.genFileID(file);
@@ -43,19 +43,13 @@ public class RestoreAction extends ActionHasReply {
     }
 
     public void run() {
-        for (int i = 0; i < chunks.size(); ++i) {
-            try {
-                restoreChannel.sendMessage(
-                    new GetchunkMsg(protocolVersion, senderID, fileID, i+1).genMsg()
-                );
-            } catch (ExceptionInInitializerError e) {
-                Utils.showError("Failed to build message, stopping delete action", this.getClass());
-                return;
-            }
+        try {
+            controlChannel.sendMessage(
+                new DeleteMsg(protocolVersion, senderID, fileID).genMsg()
+            );
+        } catch (ExceptionInInitializerError e) {
+            Utils.showError("Failed to build message, stopping delete action", this.getClass());
+            return;
         }
-    }
-
-    public void parseResponse(Message msg) {
-
     }
 }
--- Distributed-Backup-Service/src/Channel/RestoreChannel.java
+++ Distributed-Backup-Service/src/Channel/RestoreChannel.java
@@ -1,15 +1,15 @@
 package Channel;
 
 /**
- * Class to implement the RestoreAction channel, using a multicast channel
+ * Class to implement the TriggerRestoreAction channel, using a multicast channel
  */
 public class RestoreChannel extends MulticastChannel {
 
     /**
-     * RestoreAction channel constructor.
+     * TriggerRestoreAction channel constructor.
      * Defines multicast communication settings used.
      *
-     * @param channelName The channel name used to build the RestoreAction channel
+     * @param channelName The channel name used to build the TriggerRestoreAction channel
      * @param peer The peerID of the Main.Peer associated to the channel
      */
     public RestoreChannel(String channelName, Main.Peer peer) {
